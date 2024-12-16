eda2ff962aaae0a3c70f0352600282833f625b39
polinagrishko <pgrisko5@gmail.com>
2024-12-16 23:41:02 +0300

    task 6 complete

---
+++ main.py
@@ -0,0 +1,11 @@
+from collision import first,second,third,fourth
+first.isCorrectRect()
+second.isCollisionRect()
+third.intersectionAreaRect()
+rectangles = [
+    [(-3, 1), (9, 10)],
+    [(-7, 0), (13, 12)],
+    [(0, 0), (5, 5)],
+    [(2, 2), (7, 7)]
+]
+fourth.intersectionAreaMultiRect(rectangles)
